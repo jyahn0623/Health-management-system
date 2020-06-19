e789ef0f1f13e0031f914578f028eddaf2105a3a
jyahn0623 <43843665+jyahn0623@users.noreply.github.com>
2020-06-19 20:32:15 +0900

    Delete views.py

--- views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-import datetime
-
-# Create your views here.
-def welcome_msg(request):
-    return HttpResponse('migrate테스팅')
-
-def add_diet(request):
-    #계속 추가 예정...
