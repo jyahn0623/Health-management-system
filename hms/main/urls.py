de304fa085f734e25026903f1901973a19c87619
pualahn <jyahn0623@gmail.com>
2020-06-25 22:44:38 +0900

    메인화면 작업, 운동 template 연동

--- hms/main/urls.py
+++ hms/main/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.main, name="main"),
+    path("exercise", views.excercise, name="exercise"),
+    path("exercise/recommend", views.excercise_recommend, name="exercise-recommend"),
+    path("exercise/plan", views.excercise_plan, name="exercise-plan"),
 ]
